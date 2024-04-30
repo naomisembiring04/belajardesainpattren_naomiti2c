f7aed3b524e8426354814a7a2072fdf023e3744f
naomisembiring04 <163952349+naomisembiring04@users.noreply.github.com>
2024-04-30 11:30:24 +0700

    Add files via upload

---
+++ chapter 01/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+    public static void main(String[] args){
+        MallardDuck mallardDuck = new MallardDuck();
+        mallardDuck.display();
+        mallardDuck.swim();
+        mallardDuck.quack();
+        mallardDuck.fly();
+
+        System.out.println();
+
+        ToyDuck toyDuck = new ToyDuck();
+        toyDuck.display();
+        toyDuck.swim();
+        toyDuck.fly();
+    }
+}
